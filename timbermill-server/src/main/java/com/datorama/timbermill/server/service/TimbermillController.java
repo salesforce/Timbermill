c39d772d5d23d53097566ec1ca7a06dbc8f1ab65
Eden gal <egal@salesforce.com>
2020-05-16 01:15:30 +0300

    log

--- timbermill-server/src/main/java/com/datorama/timbermill/server/service/TimbermillController.java
+++ timbermill-server/src/main/java/com/datorama/timbermill/server/service/TimbermillController.java
@@ -2,7 +2,8 @@ package com.datorama.timbermill.server.service;
 
 import java.io.IOException;
 import java.util.Collection;
-import java.util.stream.Collectors;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
@@ -11,28 +12,34 @@ import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.ContentCachingRequestWrapper;
 
 import com.datorama.oss.timbermill.unit.Event;
 import com.datorama.oss.timbermill.unit.EventsWrapper;
 
-import static com.datorama.oss.timbermill.TaskIndexer.logErrorInEventsMap;
-
 @RestController
 public class TimbermillController {
 
 	private static final Logger LOG = LoggerFactory.getLogger(TimbermillController.class);
 
+	private static ExecutorService executorService;
+
 	@Autowired
 	private TimbermillService timbermillService;
 
+	public TimbermillController(@Value("${RECEIVING_THREAD:10}") int numOfThreads) {
+		executorService = Executors.newFixedThreadPool(numOfThreads);
+	}
+
 	@RequestMapping(method = RequestMethod.POST, value = "/events")
 	public String ingestEvents(@RequestBody @Valid EventsWrapper eventsWrapper) {
-		Collection<Event> events = eventsWrapper.getEvents();
-		logErrorInEventsMap(events.stream().collect(Collectors.groupingBy( e -> e.getTaskId())), "ingestEvents");
-		timbermillService.handleEvent(events);
-		return "Event handled";
+		executorService.submit(() -> {
+			Collection<Event> events = eventsWrapper.getEvents();
+			timbermillService.handleEvent(events);
+		});
+		return "Event received";
 	}
 
 	@ExceptionHandler(Exception.class)
