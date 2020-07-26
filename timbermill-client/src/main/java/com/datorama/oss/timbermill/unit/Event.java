551b511233e6779078ea6aa0256f83b84bdf21f5
Eden gal <egal@salesforce.com>
2020-07-26 12:18:30 +0300

    NPE fix

--- timbermill-client/src/main/java/com/datorama/oss/timbermill/unit/Event.java
+++ timbermill-client/src/main/java/com/datorama/oss/timbermill/unit/Event.java
@@ -284,7 +284,14 @@ public abstract class Event implements Serializable {
 	private int getNumberMapSize(Map<String, Number> map) {
 		int size = 0;
 		for (Map.Entry<String, Number> entry : map.entrySet()) {
-			size += entry.getKey().length() + (Math.log10(entry.getValue().doubleValue()) + 1) + 4; // "":,
+			if(entry != null){
+				if (entry.getKey() != null){
+					size += entry.getKey().length();
+				}
+				if (entry.getValue() != null) {
+					size += (Math.log10(entry.getValue().doubleValue()) + 1) + 4; // "":,
+				}
+			}
 		}
 		return Math.max(size-1, 0); // Last ,
 	}
