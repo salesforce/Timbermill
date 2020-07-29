96b63f6ab523f0ce8453f666ae8ed5910ebe9d7e
Eden gal <egal@salesforce.com>
2020-07-29 11:53:11 +0300

    aspectj fixes

--- timbermill-server/src/main/java/com/datorama/timbermill/server/TimbermillApplication.java
+++ timbermill-server/src/main/java/com/datorama/timbermill/server/TimbermillApplication.java
@@ -6,8 +6,10 @@ import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.datorama")
 public class TimbermillApplication {
 
     @Bean
