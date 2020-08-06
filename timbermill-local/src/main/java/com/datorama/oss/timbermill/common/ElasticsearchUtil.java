d9f7af448b9cd6d2942250206b8d7a4cb1701fe6
Guy Shemer <gshemer@salesforce.com>
2020-08-06 14:23:49 +0300

    change env tag name to prevent collisions

--- timbermill-local/src/main/java/com/datorama/oss/timbermill/common/ElasticsearchUtil.java
+++ timbermill-local/src/main/java/com/datorama/oss/timbermill/common/ElasticsearchUtil.java
@@ -26,7 +26,7 @@ import static org.quartz.TriggerBuilder.newTrigger;
 
 public class ElasticsearchUtil {
 	public static final String CLIENT = "client";
-	public static final String ENVIRONMENT = "env";
+	public static final String ENVIRONMENT = "job_env";
 	public static final String DISK_HANDLER = "disk_handler";
 	public static final String EVENTS_QUEUE = "events_queue";
 	public static final String OVERFLOWED_EVENTS_QUEUE = "overflowed_events_queue";
