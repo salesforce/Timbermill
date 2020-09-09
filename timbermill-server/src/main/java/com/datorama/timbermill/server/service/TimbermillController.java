11e3cbd1722e348f772bdc339179e83cebec8cb3
ozafar <ozafar@salesforce.com>
2020-09-09 16:37:59 +0300

    DAT-67703: pr fixes

--- timbermill-server/src/main/java/com/datorama/timbermill/server/service/TimbermillController.java
+++ timbermill-server/src/main/java/com/datorama/timbermill/server/service/TimbermillController.java
@@ -1,8 +1,10 @@
 package com.datorama.timbermill.server.service;
 
 import java.io.IOException;
+import java.util.Collection;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
@@ -18,7 +20,11 @@ import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.ContentCachingRequestWrapper;
 
+import com.datorama.oss.timbermill.unit.Event;
 import com.datorama.oss.timbermill.unit.EventsList;
+import com.datorama.oss.timbermill.unit.EventsWrapper;
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
 
 @RestController
 public class TimbermillController {
@@ -26,17 +32,41 @@ public class TimbermillController {
 	private static final Logger LOG = LoggerFactory.getLogger(TimbermillController.class);
 
 	private static ExecutorService executorService;
+	private final Cache<String, String> idsCache;
 
 	@Autowired
 	private TimbermillService timbermillService;
 
 	public TimbermillController(@Value("${RECEIVING_THREAD:10}") int numOfThreads) {
+		CacheBuilder<String, String> cacheBuilder = CacheBuilder.newBuilder().weigher((key, value) -> key.length() + value.length());
+		idsCache = cacheBuilder
+				.maximumWeight(1000000) //1MB
+				.expireAfterWrite(10, TimeUnit.SECONDS)
+				.build();
 		executorService = Executors.newFixedThreadPool(numOfThreads);
 	}
 
-
 	@RequestMapping(method = RequestMethod.POST, value = "/events")
-	public String ingestEvents(@RequestBody @Valid EventsList events) {
+	public String ingestEvents(@RequestBody @Valid EventsWrapper eventsWrapper) {
+		executorService.submit(() -> {
+			String eventsId = eventsWrapper.getId();
+			if (eventsId != null){
+				if (idsCache.getIfPresent(eventsId) != null){
+					LOG.warn("Got duplicated EventsWrapper {}", eventsWrapper.getEvents());
+					return;
+				}
+				else{
+					idsCache.put(eventsId, eventsId);
+				}
+			}
+			Collection<Event> events = eventsWrapper.getEvents();
+			timbermillService.handleEvents(events);
+		});
+		return "Event received";
+	}
+
+	@RequestMapping(method = RequestMethod.POST, value = "/events2")
+	public String ingestEvents2(@RequestBody @Valid EventsList events) {
 		executorService.submit(() -> timbermillService.handleEvents(events));
 		return "Event received";
 	}
