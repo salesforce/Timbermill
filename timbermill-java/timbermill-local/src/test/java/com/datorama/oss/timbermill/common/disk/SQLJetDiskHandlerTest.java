bb971260992381b2aff3ba9f47e2434fc635ca2e
ozafar <ozafar@salesforce.com>
2020-12-02 14:48:50 +0200

    final version

--- timbermill-java/timbermill-local/src/test/java/com/datorama/oss/timbermill/common/disk/SQLJetDiskHandlerTest.java
+++ timbermill-java/timbermill-local/src/test/java/com/datorama/oss/timbermill/common/disk/SQLJetDiskHandlerTest.java
@@ -4,11 +4,8 @@ import com.datorama.oss.timbermill.ElasticsearchClient;
 import com.datorama.oss.timbermill.common.exceptions.MaximumInsertTriesException;
 import com.datorama.oss.timbermill.unit.*;
 import org.apache.commons.lang3.SerializationException;
-import org.apache.commons.lang3.SerializationUtils;
 import org.elasticsearch.action.bulk.BulkRequest;
 import org.elasticsearch.action.update.UpdateRequest;
-import org.elasticsearch.common.io.stream.BytesStreamOutput;
-import org.elasticsearch.common.io.stream.StreamInput;
 import org.elasticsearch.script.Script;
 import org.elasticsearch.script.ScriptType;
 import org.junit.After;
@@ -17,13 +14,10 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import org.tmatesoft.sqljet.core.SqlJetException;
 
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.net.URISyntaxException;
+import java.lang.reflect.Field;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.time.ZonedDateTime;
 import java.util.*;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -38,6 +32,8 @@ public class SQLJetDiskHandlerTest {
 	private static int maxFetchedBulks = 10;
 	private static String flowId = "test";
 	private int bulkNum = 1;
+	private static String OLD_VERSION_EVENT = "/old_version_event";
+	private static String OLD_VERSION_BULK_REQUEST = "/old_version_bulk_request";
 
 	@BeforeClass
 	public static void init()  {
@@ -179,49 +175,12 @@ public class SQLJetDiskHandlerTest {
 	}
 
 	@Test
-	public void validateEventsSerialization() throws IOException, URISyntaxException {
+	public void validateBulkRequestsChanges() throws Exception {
 
+		// Checking if can deserialize previous version of BulkRequest
 		boolean deserializationSuccess = true;
 
-		String taskId = "taskId";
-		String name = "name";
-		String parentId = "parentId";
-		TaskStatus taskStatus = TaskStatus.SUCCESS;
-		String env = "env";
-		String primaryId = "primaryId";
-
-		/* Checking if no */
-		Path path = Paths.get(SQLJetDiskHandlerTest.class.getResource("/old_version_event").toURI());
-		List<Event> oldVersionEvents = null;
-		byte[] oldVersionEventBytes = Files.readAllBytes(path);
-		try {
-			oldVersionEvents = sqlJetDiskHandler.deserializeEvents(oldVersionEventBytes);
-		} catch (SerializationException e) {
-			deserializationSuccess = false;
-		}
-		assertTrue("",deserializationSuccess);
-
-		SpotEvent spotEvent = (SpotEvent) oldVersionEvents.get(0);
-		String message = "A field in Event class that was changed will break the connection with the db, field name: ";
-		assertEquals(message + "taskId", taskId, spotEvent.getTaskId());
-		assertEquals(message + "name", name, spotEvent.getName());
-		assertEquals(message + "primaryId", primaryId, spotEvent.getPrimaryId());
-		assertEquals(message + "parentId", parentId, spotEvent.getParentId());
-		assertEquals(message + "env", env, spotEvent.getEnv());
-		assertEquals(message + "status", taskStatus, spotEvent.getStatus());
-		assertNotEquals(message + "context", spotEvent.getContext(), null);
-		assertNotEquals(message + "metrics", spotEvent.getMetrics(), null);
-		assertNotEquals(message + "strings", spotEvent.getStrings(), null);
-		assertNotEquals(message + "text", spotEvent.getText(), null);
-		assertNotEquals(message + "parentsPath", spotEvent.getParentsPath(), null);
-	}
-
-	@Test
-	public void validateBulkRequestsSerialization() throws IOException, URISyntaxException {
-
-		boolean deserializationSuccess = true;
-
-		Path path = Paths.get(SQLJetDiskHandlerTest.class.getResource("/old_version_bulk_request").toURI());
+		Path path = Paths.get(SQLJetDiskHandlerTest.class.getResource(OLD_VERSION_BULK_REQUEST).toURI());
 		BulkRequest oldVersionBulk = null;
 		byte[] oldVersionBulkBytes = Files.readAllBytes(path);
 		try {
@@ -229,28 +188,95 @@ public class SQLJetDiskHandlerTest {
 		} catch (SerializationException e) {
 			deserializationSuccess = false;
 		}
+
 		assertTrue(deserializationSuccess);
 
 		String message = "A field in BulkRequest class that was changed will break the connection with the db, field name: ";
 		assertEquals(message + "taskId", 3, oldVersionBulk.numberOfActions());
 	}
 
+	@Test
+	public void validateEventsDeserialization() throws Exception {
+		// Checking if can deserialize previous version of Event
+		boolean deserializationSuccess = true;
+
+		Path path = Paths.get(SQLJetDiskHandlerTest.class.getResource(OLD_VERSION_EVENT).toURI());
+		byte[] oldVersionEventBytes = Files.readAllBytes(path);
+		try {
+			sqlJetDiskHandler.deserializeEvents(oldVersionEventBytes);
+		} catch (SerializationException e) {
+			deserializationSuccess = false;
+		}
+
+		String message = "Failed to deserialize previous version of Event, which may break " +
+				"the connection with the db.\nYou may changed a field's type.";
+		assertTrue(message, deserializationSuccess);
+	}
+
+	@Test
+	public void validateEventsFieldNames() {
+		/* Changing fields' names of Event class will pass validateEventsDeserialization test,
+		but events in the db will be deserialized as null (since the new fields don't exist there).
+		Therefore need to alert if one of Event's fields has been renamed */
+		String renameError = "A field's name in Event class that was changed will break the connection with the db, field name: ";
+		List<Class> eventClassList = new ArrayList<>(Arrays.asList(SpotEvent.class, StartEvent.class, SuccessEvent.class, ErrorEvent.class, InfoEvent.class));
+		for (Class eventSubClass : eventClassList){
+			Set<String> oldFields = getOldFields(eventSubClass);
+			Set<String> newFields = getFields(eventSubClass);
+			for (String OldField : oldFields) {
+				assertTrue(renameError + OldField + " ,Event class: " + eventSubClass.getName(), newFields.contains(OldField));
+			}
+		}
+	}
+
 	// region Test Helpers
+
 	private void fetchAndPersist() {
-        if (sqlJetDiskHandler.hasFailedBulks(flowId)) {
-            List<DbBulkRequest> failedRequestsFromDisk = sqlJetDiskHandler.fetchAndDeleteFailedBulks(flowId);
-            if (failedRequestsFromDisk.size() == 0) {
-                return;
-            }
-            for (DbBulkRequest dbBulkRequest : failedRequestsFromDisk) {
-                try {
-                    sqlJetDiskHandler.persistBulkRequestToDisk(dbBulkRequest, flowId, bulkNum);
-                } catch (MaximumInsertTriesException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
+		if (sqlJetDiskHandler.hasFailedBulks(flowId)) {
+			List<DbBulkRequest> failedRequestsFromDisk = sqlJetDiskHandler.fetchAndDeleteFailedBulks(flowId);
+			if (failedRequestsFromDisk.size() == 0) {
+				return;
+			}
+			for (DbBulkRequest dbBulkRequest : failedRequestsFromDisk) {
+				try {
+					sqlJetDiskHandler.persistBulkRequestToDisk(dbBulkRequest, flowId, bulkNum);
+				} catch (MaximumInsertTriesException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+	private Set<String> getOldFields(Class<? extends Event> clazz) {
+		HashSet<String> oldFields = new HashSet<>();
+		oldFields.add("taskId");
+		oldFields.add("name");
+		oldFields.add("primaryId");
+		oldFields.add("parentId");
+		oldFields.add("env");
+		oldFields.add("time");
+		oldFields.add("dateToDelete");
+		oldFields.add("context");
+		oldFields.add("metrics");
+		oldFields.add("strings");
+		oldFields.add("text");
+		oldFields.add("parentsPath");
+		if (clazz == SpotEvent.class){
+			oldFields.add("status");
+		}
+		return oldFields;
+	}
+
+	private Set<String> getFields(Class clazz) {
+		Set<String> fields = new HashSet<>();
+		while (clazz.getSuperclass() != null) {
+			for (Field field : clazz.getDeclaredFields()){
+				fields.add(field.getName());
+			}
+			clazz = clazz.getSuperclass();
+		}
+		return fields;
+	}
 
 	private String getRequestAsString(DbBulkRequest dbBulkRequest) {
 		return dbBulkRequest.getRequest().requests().get(0).toString();
