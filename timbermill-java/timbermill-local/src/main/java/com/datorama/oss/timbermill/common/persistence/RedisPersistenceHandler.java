b406a68715a34e69f947f8d7b4d3153625219419
ozafar <ozafar@salesforce.com>
2021-04-28 17:16:18 +0300

    DAT-75398: PR fix

--- timbermill-java/timbermill-local/src/main/java/com/datorama/oss/timbermill/common/persistence/RedisPersistenceHandler.java
+++ timbermill-java/timbermill-local/src/main/java/com/datorama/oss/timbermill/common/persistence/RedisPersistenceHandler.java
@@ -159,7 +159,7 @@ public class RedisPersistenceHandler extends PersistenceHandler {
             if (!(redisService.pushToRedisSortedSet(FAILED_BULKS_QUEUE_NAME, key, Instant.now().getEpochSecond()) && redisService.pushToRedis(map, ttl))) {
                 LOG.error("Failed to persist bulk request number {} to Redis", bulkNum);
             } else {
-                LOG.warn("Bulk #{} Bulk request was pushed to Redis.", bulkNum);
+                LOG.info("Bulk #{} Bulk request was pushed successfully to Redis.", bulkNum);
             }
         });
     }
@@ -171,7 +171,7 @@ public class RedisPersistenceHandler extends PersistenceHandler {
         if (!(redisService.pushToRedisSortedSet(OVERFLOWED_EVENTS_QUEUE_NAME, key, Instant.now().getEpochSecond()) && redisService.pushToRedis(map, ttl))) {
             LOG.error("Failed to persist overflowed events list to Redis");
         }  else {
-            LOG.warn("List of {} overflowed events was pushed to Redis.", events.size());
+            LOG.info("List of {} overflowed events was pushed successfully to Redis.", events.size());
         }
     }
 
