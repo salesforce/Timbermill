d3a718d205e5cbb88afb3f12f3d510e9e0c850cb
varestov <varestov@salesforce.com>
2023-07-23 15:04:20 +0300

    Handle Events tests

--- timbermill-java/timbermill-server/src/test/java/com/datorama/timbermill/server/service/TimbermillServiceTest.java
+++ timbermill-java/timbermill-server/src/test/java/com/datorama/timbermill/server/service/TimbermillServiceTest.java
@@ -3,97 +3,136 @@ package com.datorama.timbermill.server.service;
 import com.datorama.oss.timbermill.TimberLogger;
 import com.datorama.oss.timbermill.annotation.TimberLogTask;
 import com.datorama.oss.timbermill.common.persistence.PersistenceHandler;
+import com.datorama.oss.timbermill.common.ratelimiter.RateLimiterUtil;
 import com.datorama.oss.timbermill.unit.Event;
 import com.datorama.oss.timbermill.unit.LogParams;
 import com.datorama.oss.timbermill.unit.StartEvent;
-import com.google.common.cache.CacheStats;
 import com.google.common.cache.LoadingCache;
-import com.google.common.collect.ImmutableMap;
 import io.github.resilience4j.ratelimiter.RateLimiter;
 import junit.framework.TestCase;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.test.util.ReflectionTestUtils;
-import com.datorama.oss.timbermill.common.ratelimiter.RateLimiterUtil;
 
-import javax.annotation.CheckForNull;
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.Map;
-import java.util.concurrent.*;
-import java.time.Duration;
-
-import static org.junit.Assert.assertTrue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 
-public class TimbermillServiceTest {
+
+public class TimbermillServiceTest extends TestCase {
 
     private static TimbermillService service = Mockito.mock(TimbermillService.class, Mockito.CALLS_REAL_METHODS);
-//    PersistenceHandler persistenceHandler = Mockito.mock(PersistenceHandler.class);
-//    ReflectionTestUtils.setField(service, "persistenceHandler", persistenceHandler);
-//
-//    BlockingQueue<Event> eventsQueue = new LinkedBlockingQueue<>(CAPACITY);
-//    ReflectionTestUtils.setField(service, "eventsQueue", eventsQueue);
-//
-//    BlockingQueue<Event> overflowedQueue = new LinkedBlockingQueue<>(CAPACITY);
-//    ReflectionTestUtils.setField(service, "overflowedQueue", overflowedQueue);
-//
-//    LoadingCache<String, RateLimiter> rateLimiterMap = RateLimiterUtil.initRateLimiter(LIMIT_FOR_PERIOD, Duration.ofMinutes(LIMIT_REFRESH_PERIOD_MINUTES), RATE_LIMITER_CAPACITY);
-//    ReflectionTestUtils.setField(service, "rateLimiterMap", rateLimiterMap);
 
-    static PersistenceHandler persistenceHandler;
-    static BlockingQueue<Event> eventsQueue;
-    BlockingQueue<Event> overflowedQueue;
-    LoadingCache<String, RateLimiter> rateLimiterMap;
-
-    static final String EVENT = "Event";
-    static final int CAPACITY = 1;
+    static final String EVENT = "event";
+    static final String KEEP = "keep_event";
+    static final String SKIP = "skip_event";
+    static final int CAPACITY = 10;
 
     static final int LIMIT_FOR_PERIOD = 30000;
     static final int LIMIT_REFRESH_PERIOD_MINUTES = 1;
     static final int RATE_LIMITER_CAPACITY = 10;
 
+    static PersistenceHandler persistenceHandler = Mockito.mock(PersistenceHandler.class);
+    static BlockingQueue<Event> eventsQueue = new LinkedBlockingQueue<>(CAPACITY);
+    BlockingQueue<Event> overflowedQueue = new LinkedBlockingQueue<>(CAPACITY);
+    LoadingCache<String, RateLimiter> rateLimiterMap = RateLimiterUtil.initRateLimiter(LIMIT_FOR_PERIOD, Duration.ofMinutes(LIMIT_REFRESH_PERIOD_MINUTES), RATE_LIMITER_CAPACITY);
 
-    @BeforeAll
+
     public static void init() {
 
         // Initialize the fields you need for your test
 //        ReflectionTestUtils.setField(service, "skipEventsFlag", "true");
 //        ReflectionTestUtils.setField(service, "notToSkipRegex", ".*TestEvent.*");
 //        final TimbermillService service = Mockito.mock(TimbermillService.class, Mockito.CALLS_REAL_METHODS);
-
-        PersistenceHandler persistenceHandler = Mockito.mock(PersistenceHandler.class);
-        ReflectionTestUtils.setField(service, "persistenceHandler", persistenceHandler);
-
-        BlockingQueue<Event> eventsQueue = new LinkedBlockingQueue<>(CAPACITY);
-        ReflectionTestUtils.setField(service, "eventsQueue", eventsQueue);
-
-        BlockingQueue<Event> overflowedQueue = new LinkedBlockingQueue<>(CAPACITY);
-        ReflectionTestUtils.setField(service, "overflowedQueue", overflowedQueue);
-
-        LoadingCache<String, RateLimiter> rateLimiterMap = RateLimiterUtil.initRateLimiter(LIMIT_FOR_PERIOD, Duration.ofMinutes(LIMIT_REFRESH_PERIOD_MINUTES), RATE_LIMITER_CAPACITY);
-        ReflectionTestUtils.setField(service, "rateLimiterMap", rateLimiterMap);
+        System.out.print("Init()");
 
     }
 
-    @Test
-//    @TimberLogTask(name = EVENT)
-    public void testHandleEvents() {
+    @TimberLogTask(name = EVENT)
+    public void testHandleEventsOneMatchingTask() {
+
+        ReflectionTestUtils.setField(service, "persistenceHandler", persistenceHandler);
+        ReflectionTestUtils.setField(service, "eventsQueue", eventsQueue);
+        ReflectionTestUtils.setField(service, "overflowedQueue", overflowedQueue);
+        ReflectionTestUtils.setField(service, "rateLimiterMap", rateLimiterMap);
 
         ReflectionTestUtils.setField(service, "skipEventsFlag", "true");
-        ReflectionTestUtils.setField(service, "notToSkipRegex", "E.*");
+        ReflectionTestUtils.setField(service, "notToSkipRegex", ".*keep.*");
 
-        Event event = new StartEvent(TimberLogger.getCurrentTaskId(), EVENT, new LogParams(), null);
+        Event event = new StartEvent(TimberLogger.getCurrentTaskId(), KEEP, new LogParams(), null);
 
         service.handleEvents(Collections.singletonList(event));
 
         assertTrue(eventsQueue.contains(event));
-
     }
 
+    @TimberLogTask(name = EVENT)
+    public void testHandleEventsOneNotMatchingTask() {
+
+        ReflectionTestUtils.setField(service, "persistenceHandler", persistenceHandler);
+        ReflectionTestUtils.setField(service, "eventsQueue", eventsQueue);
+        ReflectionTestUtils.setField(service, "overflowedQueue", overflowedQueue);
+        ReflectionTestUtils.setField(service, "rateLimiterMap", rateLimiterMap);
+
+        ReflectionTestUtils.setField(service, "skipEventsFlag", "true");
+        ReflectionTestUtils.setField(service, "notToSkipRegex", ".*keep.*");
+
+        Event event = new StartEvent(TimberLogger.getCurrentTaskId(), SKIP, new LogParams(), null);
+
+        service.handleEvents(Collections.singletonList(event));
+
+        assertFalse(eventsQueue.contains(event));
+    }
+
+    @TimberLogTask(name = EVENT)
+    public void testHandleEventsMultipleTasks() {
+
+        ReflectionTestUtils.setField(service, "persistenceHandler", persistenceHandler);
+        ReflectionTestUtils.setField(service, "eventsQueue", eventsQueue);
+        ReflectionTestUtils.setField(service, "overflowedQueue", overflowedQueue);
+        ReflectionTestUtils.setField(service, "rateLimiterMap", rateLimiterMap);
+
+        ReflectionTestUtils.setField(service, "skipEventsFlag", "true");
+        ReflectionTestUtils.setField(service, "notToSkipRegex", ".keep.*");
+
+        Event eventToKeep1 = new StartEvent(TimberLogger.getCurrentTaskId(), KEEP + "1", new LogParams(), null);
+        Event eventToKeep2 = new StartEvent(TimberLogger.getCurrentTaskId(), KEEP + "2", new LogParams(), null);
+        Event eventToSkip1 = new StartEvent(TimberLogger.getCurrentTaskId(), SKIP + "1", new LogParams(), null);
+        Event eventToSkip2 = new StartEvent(TimberLogger.getCurrentTaskId(), SKIP + "1", new LogParams(), null);
+
+
+        service.handleEvents(Arrays.asList(eventToKeep1, eventToSkip1, eventToKeep2, eventToSkip2));
+
+        assertTrue(eventsQueue.contains(eventToKeep1));
+        assertTrue(eventsQueue.contains(eventToKeep2));
+        assertFalse(eventsQueue.contains(eventToSkip1));
+        assertFalse(eventsQueue.contains(eventToSkip2));
+    }
+
+    @TimberLogTask(name = EVENT)
+    public void testHandleEventsMultipleTasksFlagClosed() {
+
+        ReflectionTestUtils.setField(service, "persistenceHandler", persistenceHandler);
+        ReflectionTestUtils.setField(service, "eventsQueue", eventsQueue);
+        ReflectionTestUtils.setField(service, "overflowedQueue", overflowedQueue);
+        ReflectionTestUtils.setField(service, "rateLimiterMap", rateLimiterMap);
+
+        ReflectionTestUtils.setField(service, "skipEventsFlag", "false");
+        ReflectionTestUtils.setField(service, "notToSkipRegex", ".keep.*");
+
+        Event eventToKeep1 = new StartEvent(TimberLogger.getCurrentTaskId(), KEEP + "1", new LogParams(), null);
+        Event eventToKeep2 = new StartEvent(TimberLogger.getCurrentTaskId(), KEEP + "2", new LogParams(), null);
+        Event eventToSkip1 = new StartEvent(TimberLogger.getCurrentTaskId(), SKIP + "1", new LogParams(), null);
+        Event eventToSkip2 = new StartEvent(TimberLogger.getCurrentTaskId(), SKIP + "1", new LogParams(), null);
+
+
+        service.handleEvents(Arrays.asList(eventToKeep1, eventToSkip1, eventToKeep2, eventToSkip2));
+
+        assertTrue(eventsQueue.contains(eventToKeep1));
+        assertTrue(eventsQueue.contains(eventToKeep2));
+        assertTrue(eventsQueue.contains(eventToSkip1));
+        assertTrue(eventsQueue.contains(eventToSkip2));
+    }
 }
